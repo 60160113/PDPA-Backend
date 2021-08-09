3b2b396e40753dd80c06b0935a8e01e5d712a9c7
60160113 <60160113@go.buu.ac.th>
2021-08-09 17:15:29 +0700

    Update mongooseController.js

--- controllers/mongooseController.js
+++ controllers/mongooseController.js
@@ -24,8 +24,14 @@ module.exports = {
         try {
             const model = models[req.params.model + "Model"]
 
-            const personalData = new model(req.body)
-            const data = await personalData.save()
+            var data = null
+
+            if (Array.isArray(req.body)) {
+              data = await model.insertMany(req.body) 
+            } else {
+              data = await model.create(req.body)   
+            }
+
             res.send(data)
         } catch (error) {
             next(error)
@@ -35,9 +41,16 @@ module.exports = {
         try {
             const model = models[req.params.model + "Model"]
 
-            const data = await model.findOneAndUpdate({
-                _id: new ObjectId(req.params.id)
-            }, req.body, { new: true })
+            var data = null
+
+            if (req.params.id) {
+                data = await model.findOneAndUpdate({
+                    _id: new ObjectId(req.params.id)
+                }, req.body, { new: true })
+            } else {
+                data = await model.update(req.query, req.body, { new: true })
+            }
+
             res.send(data)
         } catch (error) {
             next(error)
@@ -49,12 +62,12 @@ module.exports = {
 
             var data = null
 
-            if (!req.params.id) {
-                data = await model.deleteMany(req.query)
-            } else {
+            if (req.params.id) {
                 data = await model.findOneAndRemove({
                     _id: new ObjectId(req.params.id)
                 })
+            } else {
+                data = await model.deleteMany(req.query)
             }
             res.send(data)
         } catch (error) {
