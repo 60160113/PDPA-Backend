6d0fa3e9b6ae69d34f10287da2f489eb2d916ddd
60160113 <60160113@go.buu.ac.th>
2021-08-10 11:29:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/mongoose', require('./routes/mongooseRoute'))
+app.use('/data', require('./routes/mongooseRoute'))
 
 app.use('/publish_data', require('./routes/publishDataRoute'))
 app.use('/expired_data', require('./routes/expiredDataRoute'))
