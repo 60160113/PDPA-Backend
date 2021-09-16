3a5b88618563e1ad2c23d7f4c7a56e0086949b3d
60160113 <60160113@go.buu.ac.th>
2021-09-16 14:56:01 +0700

    d

--- routes/publishDataRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const publishDataController = require('../controllers/publishDataController')
-
-// POST //
-router.post('/:id', publishDataController.publish)
-
-module.exports = router
