0c2e345c27aaff62351ba2a676264c71a9ce9ee7
60160113 <60160113@go.buu.ac.th>
2021-09-28 11:00:59 +0700

    Update requestModel.js

--- models/requestModel.js
+++ models/requestModel.js
@@ -2,12 +2,6 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema
 
 const requestSchema = new schema({
-    type: {
-        type: String,
-        default: "regular" // regular: request to admin to get exist files, specific: request/assign someone to send the requested files 
-    },
-
-    // common
     requester: {
         id: {
             type: String,
@@ -16,6 +10,10 @@ const requestSchema = new schema({
         name: {
             type: String,
             default: ""
+        },
+        email: {
+            type: String,
+            default: undefined
         }
     },
     folder: {
@@ -26,53 +24,18 @@ const requestSchema = new schema({
         type: String,
         default: ""
     },
+    description: {
+        type: String,
+        default: ""
+    },
+    deadline: { type: Date },
     createdAt: { type: Date, default: Date.now },
     expiredAt: { type: Date, default: undefined },
     status: {
         type: String,
         default: "pending"
     },
-
-    // regular
-    documents: [],
-    /*
-        documents: 
-        {
-            id: fileId,
-            name: fileName,
-            type: <DocumentType>
-            parent: {
-                name, id, group
-            }
-        }
-    */
-
-    // specific
-    assignTo: [], 
-    /*
-        assignTo: 
-        {
-            id: userId,
-            displayName: ...,
-            // firstName: ...,
-            // lastName: ...,
-            // email: userEmail,
-            isAccept: (boolean),
-            comment: (String)
-        }
-    */
-
-    deadline: Date
 })
-
-requestSchema.pre('save', function(next) {
-    if(this.type == "regular") {
-        this.assignTo = undefined
-    }else {
-        this.documents = undefined
-    }
-    next();
-});
 
 const request = mongoose.model("request", requestSchema, "request")
 module.exports = request
