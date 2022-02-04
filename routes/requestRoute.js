4608b1454210efa7d010e21f989aaf783ae691f9
60160113 <60160113@go.buu.ac.th>
2022-02-04 10:49:03 +0700

    update

--- routes/requestRoute.js
+++ routes/requestRoute.js
@@ -1,9 +1,6 @@
 const router = require('express').Router()
 const requestController = require('../controllers/requestController')
 
-// POST //
-router.post('/publish/:id', requestController.publish)
-
 // PUT //
 router.put('/expired/:id/:folder', requestController.forceExpiration)
 
